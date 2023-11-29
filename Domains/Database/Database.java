af76c00b1e50a277df44e50e05f39ec651b8f51b
Mohamad-Hussein <mohamadhussein15@hotmail.com>
2023-11-28 21:20:48 -0700

    add db execute method for sql commands

--- Domains/Database/Database.java
+++ Domains/Database/Database.java
@@ -6,6 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Database {
 
@@ -40,18 +42,41 @@ public class Database {
 
     }
 
-    public static void main(String[] args) {
-        
+    public static List<List<String>> dbExecute(String command) {
         Database db = new Database();
         Connection con;
+        List<List<String>> rows = new ArrayList<>();
+
         try{
             con = db.getConnection();
             Statement statement = con.createStatement();
-            ResultSet queryResult = statement.executeQuery("SELECT * FROM admin");
+            ResultSet queryResult = statement.executeQuery(command);
+            
+            // Get column names
+            int columnCount = queryResult.getMetaData().getColumnCount();
+            List<String> columnNames = new ArrayList<>();
+            for (int i = 1; i <= columnCount; i++) {
+                columnNames.add(queryResult.getMetaData().getColumnName(i));
+            }
 
-            // while (queryResult.next()) {
+            // List to store rows
 
-            // }
+            // Iterate through the result set
+            while (queryResult.next()) {
+                // List to store values in the current row
+                List<String> rowValues = new ArrayList<>();
+
+                // Retrieve values for each column in the current row
+                for (String columnName : columnNames) {
+                    String columnValue = queryResult.getString(columnName);
+                    rowValues.add(columnValue);
+                }
+
+                // Add the row to the list of rows
+                rows.add(rowValues);
+            }
+            
+            // System.out.print(rows);
 
 
         } catch(SQLException e){
@@ -59,6 +84,18 @@ public class Database {
             System.out.println("Could not get connections");
             System.exit(1);
         }
+
+        return rows;
+    }  
+
+    // Make a run function for running sql commands
+    /*
+     * This is for testing purposes
+     */
+    public static void main(String[] args) {
         
+        // This is an example
+        List<List<String>> result = dbExecute("select username from admin");
+        System.out.print(result);
     }
 }
