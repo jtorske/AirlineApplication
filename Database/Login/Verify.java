2706c2e2ffaf9a8c8fb838c749081e53199f21db
Mohamad-Hussein <mohamadhussein15@hotmail.com>
2023-11-29 08:48:57 -0700

    fix format error on query

--- Database/Login/Verify.java
+++ Database/Login/Verify.java
@@ -1,45 +1,43 @@
 package Database.Login;
+
 import java.util.List;
 
 import Database.Database.Database;
 
 public class Verify {
-    static public boolean verifyUser(String username, String password){
-        //connect to database to check if username and password are correct
-        List<List<String>> result = Database.dbExecute("select * from user"
-        + " where username = " + username
-        + " and password = " + password
-        );
-        if (result.size() > 0){
+    static public boolean verifyUser(String username, String password) {
+        // connect to database to check if username and password are correct
+        String query = String.format("SELECT * FROM User WHERE Username = \"%s\" AND Password = \"%s\"", username,
+                password);
+        List<List<String>> result = Database.dbExecute(query);
+        if (result.size() > 0) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
-    static public boolean verifyAdmin(String username, String password){
-        //connect to database to check if username and password are correct
-        List<List<String>> result = Database.dbExecute("select * from admin"
-        + " where username = " + username
-        + " and password = " + password
-        );
-        if (result.size() > 0){
+
+    static public boolean verifyAdmin(String username, String password) {
+        // connect to database to check if username and password are correct
+        String query = String.format("SELECT * FROM Admin WHERE Username = \"%s\" AND Password = \"%s\"", username,
+                password);
+        List<List<String>> result = Database.dbExecute(query);
+        if (result.size() > 0) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
-    static public boolean verifyCrewMember(String username, String password){
-        //connect to database to check if username and password are correct
-        List<List<String>> result = Database.dbExecute("select * from crewmember"
-        + " where username = " + username
-        + " and password = " + password
-        );
-        if (result.size() > 0){
+
+    static public boolean verifyCrewMember(String username, String password) {
+        // connect to database to check if username and password are correct
+        // TODO: fix this
+        String query = String.format("SELECT * FROM Admin WHERE Username = \"%s\" AND Password = \"%s\"", username,
+                password);
+        List<List<String>> result = Database.dbExecute(query);
+        if (result.size() > 0) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
