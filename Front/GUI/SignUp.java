c2093d9eb2308bc404ec7bad1f25712bdd2cf89e
Mohamad-Hussein <mohamadhussein15@hotmail.com>
2023-11-29 15:04:32 -0700

    add signup db connection

--- Front/GUI/SignUp.java
+++ Front/GUI/SignUp.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
+import Database.Database.Database;
 
 public class SignUp extends JFrame {
     private JTextField firstNameField, lastNameField, usernameField, emailField;
@@ -45,7 +47,7 @@ public class SignUp extends JFrame {
         panel.add(new JLabel());
         signUpButton = new JButton("Sign Up");
 
-        //TODO: Actual Sign Up Implementation
+        // TODO: Actual Sign Up Implementation
         signUpButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -55,11 +57,27 @@ public class SignUp extends JFrame {
                 String password = new String(passwordField.getPassword());
                 String email = emailField.getText();
 
+                List<List<String>> queryResult = 
+                    Database.dbExecute(String.format("SELECT * FROM User WHERE Username = \"%s\"", username));
+
+                System.out.print(queryResult.size());
+
+                if (queryResult.size() == 0) {
+                    // Update name table first
+                    List<String> valsName = List.of(firstName, lastName, "");
+                    String nameID = Database.dbInsert("Name (FirstName, LastName, MiddleName)", valsName);
+                    // Update user table
+                    List<String> valsUser = List.of(nameID, username, password, email);
+                    Database.dbInsert("User (NameID, Username, Password, Email)", valsUser);
+                } else {
+                    JOptionPane.showMessageDialog(SignUp.this, "Account already taken!");
+
+                }
                 // Print the gathered information to the terminal
                 System.out.println("First Name: " + firstName);
                 System.out.println("Last Name: " + lastName);
                 System.out.println("Username: " + username);
-                System.out.println("Password: " + password); 
+                System.out.println("Password: " + password);
                 System.out.println("Email: " + email);
 
                 // Placeholder for actual sign up logic
