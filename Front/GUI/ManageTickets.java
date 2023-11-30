414fe2dc404ec481559f2496ec48445959eee1cd
Jordan Torske <jordan.torske@ucalgary.ca>
2023-11-29 23:19:39 -0700

    Manage Ticket Deletion Functionality
    Now manage tickets allows logged in users to type in ID of one of their tickets to be deleted

--- Front/GUI/ManageTickets.java
+++ Front/GUI/ManageTickets.java
@@ -5,19 +5,22 @@ import Database.Database.Database;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ManageTickets extends JFrame {
-    private JTextArea resultTextArea;
-    private JTextField flightIdInput;
+    private JTextField ticketIdInput;
     private JButton selectButton;
     private String currentUser;
-
+    private JTable table; 
+    private JScrollPane scrollPane; 
+    
     public ManageTickets(String username) {
         this.currentUser = username;
         System.out.println("\nINFO:" + currentUser);
         setTitle("Manage Tickets");
-        setSize(15000, 600); 
+        setSize(15000, 600);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
         initializeComponents();
@@ -26,86 +29,98 @@ public class ManageTickets extends JFrame {
 
     private void initializeComponents() {
         setLayout(new BorderLayout());
-    
-        flightIdInput = new JTextField(20); // Use the class field instead of creating a new local variable
+
+        ticketIdInput = new JTextField(20);
         JPanel panel = new JPanel();
-        panel.add(flightIdInput);
-    
-        selectButton = new JButton("Select");
+        panel.add(ticketIdInput);
+
+        selectButton = new JButton("Cancel Flight");
         selectButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String flightId = flightIdInput.getText().trim();
-                getTicketInfo(flightId);
+                String ticketId = ticketIdInput.getText().trim(); // Get the input text
+                deleteTicket(ticketId);
             }
         });
         panel.add(selectButton);
-    
-        add(panel, BorderLayout.NORTH); // Add panel to the ManageTickets frame
+
+        add(panel, BorderLayout.NORTH);
     }
-    
+
     private void getTickets() {
+        
 
         String query = "SELECT UserID FROM User WHERE Username = \"" + currentUser + "\"";
-        
 
         List<List<String>> dbResult = Database.dbExecute(query);
-         String userID = dbResult.get(0).get(0); 
+        String userID = dbResult.get(0).get(0);
 
         String ticketQuery = "SELECT "
-                 + "t.SeatRow, "
-                 + "t.SeatColumn, "
-                 + "i.Policy, "
-                 + "s.Type AS SeatType, "
-                 + "s.Price AS SeatPrice, "
-                 + "f.DepartureDate, "
-                 + "f.ArrivalDate, "
-                 + "l.City AS Origin, "
-                 + "l2.City AS Destination "
-                 + "FROM passenger AS p "
-                 + "JOIN user AS u ON u.UserID = " + userID + " " 
-                 + "JOIN ticket AS t ON t.PassengerID = p.PassengerID "
-                 + "JOIN Insurance AS i ON t.InsuranceID = i.InsuranceID "
-                 + "JOIN SeatType AS s ON t.SeatTypeID = s.SeatTypeID "
-                 + "JOIN Flight AS f ON t.FlightID = f.FlightID "
-                 + "JOIN Location AS l ON f.DepartureLocationID = l.LocationID "
-                 + "JOIN Location AS l2 ON f.ArrivalLocationID = l2.LocationID";
+                + "t.TicketID, "
+                + "t.SeatRow, "
+                + "t.SeatColumn, "
+                + "i.Policy, "
+                + "s.Type AS SeatType, "
+                + "s.Price AS SeatPrice, "
+                + "f.DepartureDate, "
+                + "f.ArrivalDate, "
+                + "l.City AS Origin, "
+                + "l2.City AS Destination "
+                + "FROM passenger AS p "
+                + "JOIN user AS u ON u.UserID = " + userID + " "
+                + "JOIN ticket AS t ON t.PassengerID = p.PassengerID "
+                + "JOIN Insurance AS i ON t.InsuranceID = i.InsuranceID "
+                + "JOIN SeatType AS s ON t.SeatTypeID = s.SeatTypeID "
+                + "JOIN Flight AS f ON t.FlightID = f.FlightID "
+                + "JOIN Location AS l ON f.DepartureLocationID = l.LocationID "
+                + "JOIN Location AS l2 ON f.ArrivalLocationID = l2.LocationID";
 
-
-        
         List<List<String>> ticketResult = Database.dbExecute(ticketQuery);
+                if (scrollPane != null) {
+            getContentPane().remove(scrollPane); 
+        }
         displayResults(ticketResult);
     }
-    private void getTicketInfo(String flightId){
-        //wip
+
+    private void deleteTicket(String inputTicketId) {
+        if (inputTicketId != null && inputTicketId.matches("\\d+")) {
+            String checkQuery = "SELECT COUNT(*) FROM Ticket WHERE TicketID = " + inputTicketId;
+            List<List<String>> checkResult = Database.dbExecute(checkQuery);
+
+            if (!checkResult.isEmpty() && Integer.parseInt(checkResult.get(0).get(0)) > 0) {
+                Database.dbDelete("Ticket", "TicketID", inputTicketId);
+                JOptionPane.showMessageDialog(this, "Deletion Successful", "Success", JOptionPane.INFORMATION_MESSAGE);
+                getTickets();
+            } else {
+                JOptionPane.showMessageDialog(this, "No Ticket with ID " + inputTicketId + " found.", "Error",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+        } else {
+            JOptionPane.showMessageDialog(this, "Invalid Ticket ID. Please enter a valid numeric ID.", "Error",
+                    JOptionPane.ERROR_MESSAGE);
+        }
     }
+
     private void displayResults(List<List<String>> dbResult) {
-        // Define the column names for the table
         String[] columnNames = {
-            "ID", "Seat Row", "Seat Column", "Policy", "Seat Type", 
-            "Seat Price", "Departure Date", "Arrival Date", "Origin", "Destination"
+                "TicketID", "Seat Row", "Seat Column", "Policy", "Seat Type",
+                "Seat Price", "Departure Date", "Arrival Date", "Origin", "Destination"
         };
-        
-        // Create a 2D array to hold the data (with an additional column for ID)
+
         String[][] data = new String[dbResult.size()][columnNames.length];
-        
-        // Populate the data array with results, adding an ID to each row
-        int id = 0;
+
         for (int i = 0; i < dbResult.size(); i++) {
             List<String> row = dbResult.get(i);
-            data[i][0] = String.valueOf(id++); // Add the ID in the first column
             for (int j = 0; j < row.size(); j++) {
-                data[i][j + 1] = row.get(j); // Offset by one because of the ID
+                data[i][j] = row.get(j);
             }
         }
-        
-        // Create a new JTable with the data and column names
-        JTable table = new JTable(data, columnNames);
-        
-        // Replace the current JScrollPane's viewport with the new JTable
-        JScrollPane scrollPane = new JScrollPane(table);
-        this.getContentPane().add(scrollPane); // Add the new JScrollPane
+        table = new JTable(data, columnNames); 
+        scrollPane = new JScrollPane(table); 
+        getContentPane().add(scrollPane, BorderLayout.CENTER);
+
+        getContentPane().validate();
+        getContentPane().repaint();
     }
-    
 
 }
